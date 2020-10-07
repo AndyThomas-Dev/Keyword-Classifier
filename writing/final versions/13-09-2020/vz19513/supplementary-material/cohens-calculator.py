d1ba69725fcf84580750cd9e87604fab938c9780
AndyThomas-Dev <andythomas.dev@gmail.com>
2020-10-07 15:54:11 +0100

    added brief comment

--- writing/final versions/13-09-2020/vz19513/supplementary-material/cohens-calculator.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics import cohen_kappa_score
-import pandas as pd
-
-andy = pd.read_csv(r"data.csv", usecols=[5])
-joe = pd.read_csv(r"data.csv", usecols=[6])
-print(cohen_kappa_score(andy, joe))
-
